951f31aca75860d70199f49b0a2739fe736d6233
OliverOA <OliverAOakman@gmail.com>
2022-07-16 10:42:43 +1000

    ollie: Adding parameters to player dice

--- source/scenes/dice/base_dice.gd
+++ source/scenes/dice/base_dice.gd
@@ -3,11 +3,33 @@ class_name BaseDice extends RigidBody2D
 export (NodePath) onready var base_sprite = get_node(base_sprite) as Sprite
 export (NodePath) onready var face_sprite = get_node(face_sprite) as Sprite
 
+export (NodePath) onready var launch_bar = get_node(launch_bar) as TextureProgress
+
 onready var dicebag = Dicebag.new()
 
+# Effect modifiers
+var _max_power_modifier := 1.0
+var _bounce_modifier := 1.0
+var _friction_modifier := 1.0
+var _score_modifier := 1
 
+# Some variables require resetting
+var _reset_bounce: float
+var _reset_friction: float
+
+const _MAX_POWER_INCREASE = 1.5
+const _MAX_POWER_DECREASE = 0.5
+const _FRICTION_DECREASE = 0.5
+const _BOUNCE_INCREASE = 1.5
+
+# Terrain modifiers
+var _terrain_max_power_modifier := 1.0
+var _terrain_friction_modifier := 1.0
+var _terrain_linear_damp_modifier := 1.0
+
+# States
 enum State {
-	START,
+	START,  # Exit to aiming when ready
 	AIMING,  # Enables aiming UI
 	LAUNCHING,  
 	FLYING,
@@ -18,13 +40,41 @@ enum State {
 
 var _state: int = State.START
 
+# Movement variables
+const _VELOCITY_STOP_THRESHOLD = 50
+var _max_launch_force := 150
+var _slowed_enough := false
+
 
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 func _physics_process(_delta: float) -> void:
-	pass
+	_slowed_enough = linear_velocity.length() < _VELOCITY_STOP_THRESHOLD
+
+	match _state:
+		State.START:
+			pass
+
+		State.AIMING:
+			pass
+
+		State.LAUNCHING:
+			pass
+
+		State.FLYING:
+			pass
+
+		State.ROLLING:
+			pass
+
+		State.WAITING:
+			pass
+
+		State.CHANGING_PROPERTIES:
+			pass
+
 
 
 # CUSTOM METHODS
---
+++ source/scenes/dice/player_dice.gd
@@ -0,0 +1,5 @@
+class_name PlayerDice extends BaseDice
+
+
+func _ready() -> void:
+	pass
